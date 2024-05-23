2ff239612f812904ab76aebca413596761f7ec0f
sciloaf <8636286-sciloaf@users.noreply.gitlab.com>
2024-05-23 10:16:02 +0200

    WIP on adding quantized tensors support

--- brain.pas
+++ brain.pas
@@ -5,11 +5,11 @@ unit brain;
 interface
 
 uses
-  SysUtils, Math;
+  SysUtils, Math, Dialogs;
 
 type
-  PConfig = ^TConfig;
-  TConfig = record
+  PVNAConfig = ^TVNAConfig;
+  TVNAConfig = record
     dim: integer; // transformer dimension
     hidden_dim: integer; // for ffn layers
     n_layers: integer; // number of layers
@@ -17,30 +17,40 @@ type
     n_kv_heads: integer; // number of key/value heads (can be < query heads because of multiquery)
     vocab_size: integer; // vocabulary size, usually 256 (byte-level)
     seq_len: integer; // max sequence length
+    shared_classifier: integer;
+    GS: integer;
+    data_size: cardinal; // uint32
   end;
 
+  PInt8 = array of ShortInt; // int8
   PFloats = array of single;
+
+  TQuantizedTensor = record
+    q: PInt8;
+    s: PFloats;
+  end;
 
   PTransformerWeights = ^TTransformerWeights;
   TTransformerWeights = record
     // token embedding table
-    token_embedding_table: PFloats;    // (vocab_size, dim)
+    q_tokens: TQuantizedTensor; // (vocab_size, dim)
+    token_embedding_table: PFloats; // (vocab_size, dim)
     // weights for rmsnorms
     rms_att_weight: PFloats; // (layer, dim) rmsnorm weights
     rms_ffn_weight: PFloats; // (layer, dim)
     // weights for matmuls. note dim == n_heads * head_size
-    wq: PFloats; // (layer, dim, n_heads * head_size)
-    wk: PFloats; // (layer, dim, n_kv_heads * head_size)
-    wv: PFloats; // (layer, dim, n_kv_heads * head_size)
-    wo: PFloats; // (layer, n_heads * head_size, dim)
+    wq: TQuantizedTensor; // (layer, dim, n_heads * head_size)
+    wk: TQuantizedTensor; // (layer, dim, n_kv_heads * head_size)
+    wv: TQuantizedTensor; // (layer, dim, n_kv_heads * head_size)
+    wo: TQuantizedTensor; // (layer, n_heads * head_size, dim)
     // weights for ffn
-    w1: PFloats; // (layer, hidden_dim, dim)
-    w2: PFloats; // (layer, dim, hidden_dim)
-    w3: PFloats; // (layer, hidden_dim, dim)
+    w1: TQuantizedTensor; // (layer, hidden_dim, dim)
+    w2: TQuantizedTensor; // (layer, dim, hidden_dim)
+    w3: TQuantizedTensor; // (layer, hidden_dim, dim)
     // final rmsnorm
     rms_final_weight: PFloats; // (dim,)
     // (optional) classifier weights for the logits, on the last layer
-    wcls: PFloats;
+    wcls: TQuantizedTensor;
   end;
 
   PRunState = ^TRunState;
@@ -51,6 +61,8 @@ type
     xb2: PFloats; // an additional buffer just for convenience (dim,)
     hb: PFloats; // buffer for hidden dimension in the ffn (hidden_dim,)
     hb2: PFloats; // buffer for hidden dimension in the ffn (hidden_dim,)
+    xq: TQuantizedTensor; // quantized x (dim,)
+    hq: TQuantizedTensor; // quantized hb (hidden_dim,)
     q: PFloats; // query (dim,)
     k: PFloats; // key (dim,)
     v: PFloats; // value (dim,)
@@ -63,7 +75,7 @@ type
 
   PTransformer = ^TTransformer;
   TTransformer = record
-    config: TConfig; // the hyperparameters of the architecture (the blueprint)
+    config: TVNAConfig; // the hyperparameters of the architecture (the blueprint)
     weights: TTransformerWeights; // the weights of the model
     state: TRunState; // buffers for the "wave" of activations in the forward pass
   end;
@@ -74,8 +86,10 @@ type
 const
   IOBufSize = 10000;
   epsilon = 1.0e-5;
+  Q_MAX = 127.0;
 
 var
+  GS: integer;
   brain_err: string;
 
 procedure FileReadWithFail(Handle: Integer; var Buffer; Count: Integer); forward;
@@ -83,6 +97,8 @@ procedure build_transformer(fhandle: integer; t: PTransformer; callback: TLoadCa
 procedure free_transformer(t: PTransformer); forward;
 procedure softmax(var x: PFloats; off, size: integer); forward;
 function nn_ff(transformer: PTransformer; token, pos: integer): PFloats; forward;
+procedure dequantize(qx: TQuantizedTensor; var x: PFloats; n: integer); forward;
+procedure quantize(qx: TQuantizedTensor; var x: PFloats; n: integer); forward;
 
 implementation
 
@@ -103,17 +119,29 @@ begin
   for i := 0 to size-1 do o[i+out_off] := inp[i+inp_off];
 end;
 
+procedure qtensor_copy(var o: TQuantizedTensor; inp: TQuantizedTensor);
+var
+  i: integer;
+begin
+  SetLength(o.q,High(inp.q)+1);
+  SetLength(o.s,High(inp.s)+1);
+  for i := 0 to High(inp.q) do o.q[i] := inp.q[i];
+  for i := 0 to High(inp.s) do o.s[i] := inp.s[i];
+end;
+
 procedure FileReadWithFail(Handle: Integer; var Buffer; Count: Integer);
 begin
   if FileRead(Handle,Buffer,Count) <> Count then
-    raise EFileReadError.Create('File read failed: unable to read '+IntToStr(Count)+' bytes');
+    raise EFileReadError.Create('File read failed: unable to read '+IntToStr(Count)+' bytes @ 0x' + IntToHex(FileSeek(Handle,0,1),10));
 end;
 
-procedure malloc_run_state(s: PRunState; p: PConfig);
+procedure malloc_run_state(s: PRunState; p: PVNAConfig);
 var
   kv_dim: integer;
 begin
   kv_dim := round((p^.dim * p^.n_kv_heads) / p^.n_heads);
+
+  // allocate float tensors
   SetLength(s^.x,p^.dim);
   SetLength(s^.xb,p^.dim);
   SetLength(s^.xb2,p^.dim);
@@ -126,6 +154,18 @@ begin
   SetLength(s^.logits,p^.vocab_size);
   SetLength(s^.key_cache,p^.n_layers * p^.seq_len * kv_dim);
   SetLength(s^.value_cache,p^.n_layers * p^.seq_len * kv_dim);
+
+  // allocate quantized runtime tensors
+  SetLength(s^.xq.q,p^.dim);
+  SetLength(s^.xq.s,p^.dim); // shouldn't it be "div GS" ?
+  SetLength(s^.hq.q,p^.hidden_dim);
+  SetLength(s^.hq.s,p^.hidden_dim);
+end;
+
+procedure free_tensor(var z: TQuantizedTensor);
+begin
+  z.q := nil;
+  z.s := nil;
 end;
 
 procedure free_run_state(s: PRunState);
@@ -142,6 +182,8 @@ begin
   s^.logits := nil;
   s^.key_cache := nil;
   s^.value_cache := nil;
+  free_tensor(s^.xq);
+  free_tensor(s^.hq);
 end;
 
 procedure alloc_and_read(var arr: PFloats; fhandle: integer; size: cardinal; callback: TLoadCallbkFun);
@@ -177,67 +219,118 @@ begin
   end;
 end;
 
-procedure load_weights(fhandle: integer; w: PTransformerWeights; p: PConfig; shared_weights: boolean; callback: TLoadCallbkFun);
+procedure qalloc_and_qread(var arr: TQuantizedTensor; fhandle: integer; amount, size_each: integer; callback: TLoadCallbkFun);
 var
-  head_size: integer;
+  i,j: Integer;
+  nq,ns,size,r: cardinal;
+  a: array[0..IOBufSize] of ShortInt;
+  f: array[0..IOBufSize] of single;
 begin
+  SetLength(arr.q,amount*size_each);
+  SetLength(arr.s,amount*(size_each div GS));
+  nq := 0;
+  ns := 0;
+  for i := 0 to amount-1 do
+  begin
+    // read the quantized weights
+    size := size_each;
+    while size > 0 do
+    begin
+      if size > IOBufSize then r := IOBufSize
+      else r := size;
+      FileReadWithFail(fhandle,a,r*sizeof(ShortInt));
+      size := size - r;
+      for j := 0 to r-1 do
+      begin
+        arr.q[nq] := a[j];
+        inc(nq);
+      end;
+    end;
+
+    // read the scale factors
+    size := size_each div GS;
+    while size > 0 do
+    begin
+      if size > IOBufSize then r := IOBufSize
+      else r := size;
+      FileReadWithFail(fhandle,f,r*sizeof(single));
+      size := size - r;
+      for j := 0 to r-1 do
+      begin
+        arr.s[ns] := f[j];
+        inc(ns);
+      end;
+    end;
+
+    // callback for interactivity
+    if @callback <> nil then
+    begin
+      if not callback(amount,i+1) then
+        raise EFileReadError.Create('Aborted by user');
+    end;
+  end;
+end;
+
+procedure load_weights(fhandle: integer; trans: PTransformer; callback: TLoadCallbkFun);
+var
+  p: PVNAConfig;
+  w: PTransformerWeights;
+  head_size: integer;
+  tokz: cardinal;
+begin
+  p := @trans^.config;
+  w := @trans^.weights;
   head_size := p^.dim div p^.n_heads;
 
   // no comments, it's a mess. Delphi doesn't have proper pointer arithmetic, so... improvisation
-  alloc_and_read(w^.token_embedding_table,fhandle,p^.vocab_size * p^.dim,callback);
   alloc_and_read(w^.rms_att_weight,fhandle,p^.n_layers * p^.dim,callback);
-  alloc_and_read(w^.wq,fhandle,p^.n_layers * p^.dim * (p^.n_heads * head_size),callback);
-  alloc_and_read(w^.wk,fhandle,p^.n_layers * p^.dim * (p^.n_kv_heads * head_size),callback);
-  alloc_and_read(w^.wv,fhandle,p^.n_layers * p^.dim * (p^.n_kv_heads * head_size),callback);
-  alloc_and_read(w^.wo,fhandle,p^.n_layers * (p^.n_heads * head_size) * p^.dim,callback);
   alloc_and_read(w^.rms_ffn_weight,fhandle,p^.n_layers * p^.dim,callback);
-  alloc_and_read(w^.w1,fhandle,p^.n_layers * p^.dim * p^.hidden_dim,callback);
-  alloc_and_read(w^.w2,fhandle,p^.n_layers * p^.hidden_dim * p^.dim,callback);
-  alloc_and_read(w^.w3,fhandle,p^.n_layers * p^.dim * p^.hidden_dim,callback);
   alloc_and_read(w^.rms_final_weight,fhandle,p^.dim,callback);
 
-  FileSeek(fhandle,p^.seq_len * head_size div 2 * sizeof(single),1); // skip what used to be freq_cis_real (for RoPE)
-  FileSeek(fhandle,p^.seq_len * head_size div 2 * sizeof(single),1); // skip what used to be freq_cis_imag (for RoPE)
+  tokz := p^.vocab_size * p^.dim;
+  qalloc_and_qread(w^.q_tokens,fhandle,1,tokz,callback);
+  SetLength(w^.token_embedding_table,tokz);
+  dequantize(w^.q_tokens,w^.token_embedding_table,tokz);
 
-  if shared_weights then
-  begin
-    SetLength(w^.wcls,p^.vocab_size * p^.dim);
-    floatcpy_off(w^.wcls,w^.token_embedding_table,0,p^.vocab_size * p^.dim);
-  end
+  qalloc_and_qread(w^.wq,fhandle,p^.n_layers,p^.dim * (p^.n_heads * head_size),callback);
+  qalloc_and_qread(w^.wk,fhandle,p^.n_layers,p^.dim * (p^.n_kv_heads * head_size),callback);
+  qalloc_and_qread(w^.wv,fhandle,p^.n_layers,p^.dim * (p^.n_kv_heads * head_size),callback);
+  qalloc_and_qread(w^.wo,fhandle,p^.n_layers,(p^.n_heads * head_size) * p^.dim,callback);
+
+  qalloc_and_qread(w^.w1,fhandle,p^.n_layers,p^.dim * p^.hidden_dim,callback);
+  qalloc_and_qread(w^.w2,fhandle,p^.n_layers,p^.hidden_dim * p^.dim,callback);
+  ShowMessage(IntToStr(FileSeek(fhandle,0,1)));
+  qalloc_and_qread(w^.w3,fhandle,p^.n_layers,p^.dim * p^.hidden_dim,callback);
+  ShowMessage(IntToStr(FileSeek(fhandle,0,1)));
+
+  if trans^.config.shared_classifier > 0 then
+    qtensor_copy(w^.wcls,w^.q_tokens)
   else
-    alloc_and_read(w^.wcls,fhandle,p^.vocab_size * p^.dim,callback);
+    qalloc_and_qread(w^.wcls,fhandle,1,p^.vocab_size * p^.dim,callback);
 end;
 
 procedure free_weights(w: PTransformerWeights);
 begin
+  free_tensor(w^.q_tokens);
   w^.token_embedding_table := nil;
   w^.rms_att_weight := nil;
-  w^.wq := nil;
-  w^.wk := nil;
-  w^.wv := nil;
-  w^.wo := nil;
   w^.rms_ffn_weight := nil;
-  w^.w1 := nil;
-  w^.w2 := nil;
-  w^.w3 := nil;
+  free_tensor(w^.wq);
+  free_tensor(w^.wk);
+  free_tensor(w^.wv);
+  free_tensor(w^.wo);
+  free_tensor(w^.w1);
+  free_tensor(w^.w2);
+  free_tensor(w^.w3);
   w^.rms_final_weight := nil;
-  w^.wcls := nil;
-end;
-
-procedure read_checkpoint(fhandle: integer; cfg: PConfig; weights: PTransformerWeights; callback: TLoadCallbkFun);
-begin
-  FileSeek(fhandle,4,0); // skip data block size
-  // read in the config header
-  FileReadWithFail(fhandle,cfg^,sizeof(TConfig));
-  // negative vocab size is hacky way of signaling unshared weights
-  load_weights(fhandle,weights,cfg,(cfg^.vocab_size > 0),callback);
+  free_tensor(w^.wcls);
 end;
 
 procedure build_transformer(fhandle: integer; t: PTransformer; callback: TLoadCallbkFun);
 begin
-  // read in the Config and the Weights from the checkpoint
-  read_checkpoint(fhandle, @t^.config, @t^.weights,callback);
-  // allocate the RunState buffers
+  FileReadWithFail(fhandle,t^.config,sizeof(TVNAConfig));
+  GS := t^.config.GS;
+  load_weights(fhandle,t,callback);
   malloc_run_state(@t^.state, @t^.config);
 end;
 
@@ -251,6 +344,45 @@ end;
 // Mmmm... Mathematics :)
 // A word of advice - don't try to follow the offset-based arithmetic here -
 // you'll lose your mind. You've been warned! ;)
+
+procedure dequantize(qx: TQuantizedTensor; var x: PFloats; n: integer);
+var
+  i: integer;
+begin
+  for i := 0 to n-1 do
+    x[i] := qx.q[i] * qx.s[i div GS];
+end;
+
+procedure quantize(qx: TQuantizedTensor; var x: PFloats; n: integer);
+var
+  i,num_groups,group: integer;
+  wmax,val,scale,quant_value: single;
+  quantized: ShortInt;
+begin
+  num_groups := n div GS;
+  for group := 0 to num_groups-1 do
+  begin
+    // find the max absolute value in the current group
+    wmax := 0.0;
+    for i := 0 to GS-1 do
+    begin
+      val := abs(x[group * GS + i]);
+      if val > wmax then wmax := val;
+    end;
+
+    // calculate and write the scaling factor
+    scale := wmax / Q_MAX;
+    qx.s[group] := scale;
+
+    // calculate and write the quantized values
+    for i := 0 to GS-1 do
+    begin
+      quant_value := x[group * GS + i] / scale; // scale
+      quantized := round(quant_value); // round and clamp
+      qx.q[group * GS + i] := quantized;
+    end;
+  end;
+end;
 
 procedure rmsnorm(var o: PFloats; x, weight: PFloats; woff, size: integer);
 var
@@ -292,17 +424,32 @@ begin
     x[i] := x[i] / sum;
 end;
 
-procedure matmul(var xout: PFloats; x: PFloats; w: PFloats; woff, n, d: integer);
+procedure matmul(var xout: PFloats; x,w: TQuantizedTensor; woff,n,d: integer);
 var
-  i,j: integer;
+  i,j,k: integer;
   val: single;
+  ival,inoff,qa,qb: integer;
 begin
   // W (d,n) @ x (n,) -> xout (d,)
   // by far the most amount of time is spent inside this little function
   for i := 0 to d-1 do
   begin
-    val := 0;
-    for j := 0 to n-1 do val := val + w[i * n + j + woff] * x[j];
+    val := 0.0;
+    ival := 0;
+    inoff := woff + i * n;
+    j := 0;
+    while j <= n - GS do
+    begin
+      for k := 0 to GS-1 do
+      begin
+        qa := x.q[j + k];
+        qb := w.q[inoff + j + k];
+        ival := ival + (qa * qb);
+      end;
+      val := val + ival * w.s[(inoff + j) div GS] * x.s[j div GS];
+      ival := 0;
+      inc(j,GS);
+    end;
     xout[i] := val;
   end;
 end;
@@ -313,7 +460,7 @@ end;
 function nn_ff(transformer: PTransformer; token, pos: integer): PFloats;
 var
   i,j,l,h,ts: integer;
-  p: PConfig;
+  p: PVNAConfig;
   w: PTransformerWeights;
   s: PRunState;
   x,vr: PFloats;
@@ -342,9 +489,10 @@ begin
     rmsnorm(s^.xb, x, w^.rms_att_weight, l*dim, dim);
 
     // qkv matmuls for this position
-    matmul(s^.q, s^.xb, w^.wq, l*dim*dim, dim, dim);
-    matmul(s^.k, s^.xb, w^.wk, l*dim*kv_dim, dim, kv_dim);
-    matmul(s^.v, s^.xb, w^.wv, l*dim*kv_dim, dim, kv_dim);
+    quantize(s^.xq, s^.xb, dim);
+    matmul(s^.q, s^.xq, w^.wq, l*dim*dim, dim, dim);
+    matmul(s^.k, s^.xq, w^.wk, l*dim*kv_dim, dim, kv_dim);
+    matmul(s^.v, s^.xq, w^.wv, l*dim*kv_dim, dim, kv_dim);
 
     // RoPE relative positional encoding: complex-valued rotate q and k in each head
     i := 0;
@@ -417,7 +565,8 @@ begin
     end;
 
     // final matmul to get the output of the attention
-    matmul(s^.xb2, s^.xb, w^.wo, l*dim*dim, dim, dim);
+    quantize(s^.xq, s^.xb, dim);
+    matmul(s^.xb2, s^.xq, w^.wo, l*dim*dim, dim, dim);
 
     // residual connection back into x
     for i := 0 to dim-1 do x[i] := x[i] + s^.xb2[i];
@@ -427,8 +576,9 @@ begin
 
     // Now for FFN in PyTorch we have: self.w2(F.silu(self.w1(x)) * self.w3(x))
     // first calculate self.w1(x) and self.w3(x)
-    matmul(s^.hb, s^.xb, w^.w1, l*dim*hidden_dim, dim, hidden_dim);
-    matmul(s^.hb2, s^.xb, w^.w3, l*dim*hidden_dim, dim, hidden_dim);
+    quantize(s^.xq, s^.xb, dim);
+    matmul(s^.hb, s^.xq, w^.w1, l*dim*hidden_dim, dim, hidden_dim);
+    matmul(s^.hb2, s^.xq, w^.w3, l*dim*hidden_dim, dim, hidden_dim);
 
     // SwiGLU non-linearity
     for i := 0 to hidden_dim-1 do
@@ -442,7 +592,8 @@ begin
     end;
 
     // final matmul to get the output of the ffn
-    matmul(s^.xb, s^.hb, w^.w2, l*dim*hidden_dim, hidden_dim, dim);
+    quantize(s^.hq, s^.hb, dim);
+    matmul(s^.xb, s^.hq, w^.w2, l*dim*hidden_dim, hidden_dim, dim);
 
     // residual connection
     for i := 0 to dim-1 do x[i] := x[i] + s^.xb[i];
@@ -452,7 +603,8 @@ begin
   rmsnorm(x, x, w^.rms_final_weight, 0, dim);
 
   // classifier into logits
-  matmul(s^.logits, x, w^.wcls, 0, p^.dim, p^.vocab_size);
+  quantize(s^.xq, s^.x, dim);
+  matmul(s^.logits, s^.xq, w^.wcls, 0, p^.dim, p^.vocab_size);
   Result := s^.logits;
 end;
 
